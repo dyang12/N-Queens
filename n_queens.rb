3fb07b27489dc37b9a4c7288fe6aee84e92f33f1
Dean <dean.yang02@gmail.com>
2013-11-19 12:57:06 -0800

    change to n-queens solution

--- n_queens.rb
+++ n_queens.rb
@@ -1,24 +1,28 @@
-class EightQueens
+class NQueens
   
   attr_accessor :board
   
-  def initialize
-    @board = Array.new(8) { Array.new(8) }
+  def initialize(n)
+    @board = Array.new(n) { Array.new(n) }
+    @size = n
+    @solutions = 0
     @iterations = 0
     put_queen(0)
-    puts @iterations
+    puts "number of iterations: #{@iterations}"
+    puts "number of solutions: #{@solutions}"
   end
   
   def put_queen(row)
-    @board[row].length.times do |i|
+    @size.times do |i|
       @iterations += 1
       if is_valid?([row, i])
         @board[row][i] = "Q"
       
-        if row < 7
+        if row < @board.length-1
           put_queen(row+1)
         else
           display_solution
+          @solutions += 1
         end
         
         @board[row][i] = nil
@@ -64,18 +68,18 @@ class EightQueens
   end
   
   def out_of_bounds?(pos)
-    pos[0] < 0 || pos[1] < 0 || pos[0] > 7 || pos[1] > 7
+    pos[0] < 0 || pos[1] < 0 || pos[0] > @size-1 || pos[1] > @size-1
   end
   
   def display_solution
     flat_board = @board.flatten
     queen_positions = []
     
-    8.times do
+    @size.times do
       index = flat_board.index("Q")
       flat_board[index] = nil
       
-      queen_positions << [index/8, index%8]
+      queen_positions << [index/@size, index % @size]
     end
     
     puts queen_positions.map { |pos| pos.to_s }.join(", ")
